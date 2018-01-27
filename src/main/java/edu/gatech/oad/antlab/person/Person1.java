369a6a572c970626e0d40dce81e23965f0186c29
Sam <sam.scheele@outlook.com>
2018-01-27 18:01:51 -0500

    Implemented rotation for Person1

--- src/main/java/edu/gatech/oad/antlab/person/Person1.java
+++ src/main/java/edu/gatech/oad/antlab/person/Person1.java
@@ -31,7 +31,11 @@ public class Person1 {
 	 */
 	private String calc(String input) {
 	  //Person 1 put your implementation here
-	  return null;
+	  	String retVal = "";
+		for (int i = 0; i < input.length(); i++) {
+			retVal += (i + 2) % input.length();
+		}
+		return retVal;
 	}
 	
 	/**
